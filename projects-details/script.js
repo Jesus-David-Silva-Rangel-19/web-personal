87656264ca0524443061654058768926fc0a449b
Jesús David Silva Rangel <121721345+Jesus-David-Silva-Rangel-19@users.noreply.github.com>
2024-11-04 15:26:21 +0000

    si

---
+++ projects-details/script.js
@@ -0,0 +1,9 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    Swal.fire({
+        title: 'Monitor Digital de Presión Arterial',
+        text: 'MDPA permite a los usuarios realizar un seguimiento de su presión arterial en tiempo real, con alertas y gráficos personalizables.',
+        icon: 'info',
+        confirmButtonText: 'Entendido',
+        confirmButtonColor: '#d9534f'
+    });
+});
